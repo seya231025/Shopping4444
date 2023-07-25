91d46014ad7bd108b0c19275108f15e6dc5c03b3
Seiya Iuchi <2311025@st.yoshida-g.ac.jp>
2023-07-25 11:14:39 +0900

    first commit

---
+++ src/main/java/jp/yoshida/shop/Shopping4444Application.java
@@ -0,0 +1,13 @@
+package jp.yoshida.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shopping4444Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shopping4444Application.class, args);
+	}
+
+}
---
+++ src/test/java/jp/yoshida/shop/Shopping4444ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.yoshida.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shopping4444ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
